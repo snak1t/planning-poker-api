f7cd5c9e6cd2cec794c9a272d6ae954149b63517
Ruslan Abramov <r.o.abramov@gmail.com>
2018-12-20 16:12:26 +0300

    Fix env variables

---
+++ ormconfig.js
@@ -0,0 +1,14 @@
+module.exports = {
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: ['./**/*.entity{.ts,.js}'],
+  migrationsTableName: 'custom_migration_table',
+  migrations: ['./migration/*.ts'],
+  cli: {
+    migrationsDir: 'migration',
+  },
+};
